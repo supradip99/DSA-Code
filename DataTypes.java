70a209743d25f93239b4e6a6524b4805aaeb4a3a
supradip99 <supradip.karfa99@gmail.com>
2023-07-03 13:48:17 +0530

    This is dataType program

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		String s="Ram";
+		double d=1.4;
+		
+		System.out.println(a+" Integer");
+		System.out.println(s+" String");
+		System.out.println(d+" Double");
+
+	}
+
+}
